975f66eba9e97c448f95068e86196b4db67d86e7
hitanshi <hitanshimehta18@gmail.com>
2020-02-24 23:30:04 +0530

    Jakhas

--- practiceapp/views/UserView.py
+++ practiceapp/views/UserView.py
@@ -1,19 +1,14 @@
+from django.http import HttpResponse, JsonResponse
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
 
-from practiceapp.Serializers.UserSerializer import UserSerializer
 
 
 class UserView(APIView):
     def get(self,request):
-        UserData=User.objects.filter(username__contains=request.GET.get('username'))
-        UserDict=[]
-        for user in UserData:
-            Id=User.objects.get(username=user).pk
-            UserDict.append({"Id":Id,"UserName":user})
-            #UserDict.update({"Id":Id,"UserName":user})
-        print("UserDict",UserDict)
-        return Response()
+        UserData=User.objects.filter(username__contains=request.GET.get('username')).values()
+        return JsonResponse({"User": list(UserData)})
 
 
